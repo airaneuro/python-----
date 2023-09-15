65167cf2cbdd478bf819ee3e42b9a4f3000eafac
airaneuro <67913804+airaneuro@users.noreply.github.com>
2023-09-15 11:05:35 +0900

    commit

--- streamlit_gui.py
+++ streamlit_gui.py
@@ -36,12 +36,12 @@ def attribute_query(q1, isRequired=None):
 def main():
     # Streamlit UI
     st.title("属性定義書ナビ")
-    genre_hierarchys = st.sidebar.multiselect('ジャンル階層名', get_distinct_values("ジャンル階層名"), max_selections=1)
+    genre_hierarchys = st.sidebar.multiselect('ジャンル階層名', get_distinct_values("ジャンル階層名"))
 
     if genre_hierarchys:
         isRequired = st.sidebar.checkbox('必須/任意')
         attribute_names = st.sidebar.multiselect(
-            '項目名', attribute_query(genre_hierarchys[0], isRequired), max_selections=1)
+            '項目名', attribute_query(genre_hierarchys[0], isRequired))
 
         if attribute_names:
             attribute_details = search_attribute_details(genre_hierarchys[0], attribute_names[0])
